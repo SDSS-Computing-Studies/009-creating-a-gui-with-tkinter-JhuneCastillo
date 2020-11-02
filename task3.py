4cf84e0ad11c493a0b504295444b5437514ab782
JhuneCastillo <71406970+JhuneCastillo@users.noreply.github.com>
2020-11-02 12:07:38 -0800

    .

--- task3.py
+++ task3.py
@@ -8,13 +8,15 @@ window.title("Example")
 dogphoto = PhotoImage(file="dog.png")
 label1 = tk.Label(window, image=dogphoto)
 label2 = tk.Label(window, text="Pochacco!")
-label3 = tk.Label(window, text=" A cuddly little puppy! This is from the same ", bg="#aaffff")
-label4 = tk.Label(window, text="creators who bought you Keropi and Kero Kero", bg="#aaffff")
+label3 = tk.Label(
+    window, text=" A cuddly little puppy! This is from the same ", bg="#aaffff")
+label4 = tk.Label(
+    window, text="creators who bought you Keropi and Kero Kero", bg="#aaffff")
 
 
-label1.grid(row=1, column=2, rowspan=3)
-label2.grid(row=2, column=3)
-label3.grid(row=4, column=2)
-label4.grid(row=5, column=2)
+label1.grid(row=1, column=1, rowspan=3, columnspan=3)
+label2.grid(row=2, column=3, columnspan=2)
+label3.grid(row=4, column=2, columnspan=3)
+label4.grid(row=5, column=2, columnspan=3)
 
 window.mainloop()
--- task4.py
+++ task4.py
@@ -4,11 +4,20 @@ from tkinter import ttk
 
 window = tk.Tk()
 window.title("Example")
+window.geometry("300x200")
+
 
 dogphoto = PhotoImage(file="dog.png")
 label1 = tk.Label(window, image=dogphoto)
 label2 = tk.Label(window, text="Pochacco!")
-label3 = tk.Label(window, text=" A cuddly little puppy! This is from the same ", bg="#aaffff")
-label4 = tk.Label(window, text="creators who bought you Keropi and Kero Kero", bg="#aaffff")
+label3 = tk.Label(
+    window, text=" A cuddly little puppy! This is from the same ", bg="#aaffff")
+label4 = tk.Label(
+    window, text="creators who bought you Keropi and Kero Kero", bg="#aaffff")
 
-label1.Place()
+label1.place(x=70, y=50)
+label2.place(x=140, y=85)
+label3.place(x=15, y=150)
+label4.place(x=15, y=170)
+
+window.mainloop()
